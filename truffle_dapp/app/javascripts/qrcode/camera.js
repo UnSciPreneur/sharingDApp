b6e77209a0c7acc476c99bd3ab85b3de027caa76
Andreas Bogner <abogner@ethz.ch>
2016-05-23 19:05:17 +0200

    fiddling with front and back camera

--- truffle_dapp/app/javascripts/qrcode/camera.js
+++ truffle_dapp/app/javascripts/qrcode/camera.js
@@ -6,59 +6,60 @@
 
 var camera = (function (p_vid_id, p_inter, p_scale) {
 
-    if (p_vid_id == undefined) {
-        console.log("ERROR: You need to specify the id of the <video> element with the camera data stream.");
-        return;
+  if (p_vid_id == undefined) {
+    console.log("ERROR: You need to specify the id of the <video> element with the camera data stream.");
+    return;
+  }
+
+  var vid_id = p_vid_id;
+  var interval = p_inter != undefined ? p_inter : 1000;
+  var scale = p_scale != undefined ? p_scale : 0.5;
+
+  var video = document.getElementById(vid_id);
+  var int_id = null;
+
+  function start() {
+    int_id = setInterval(function (video, scale) {
+      capture()
+    }, interval);
+  }
+
+  function stop() {
+    console.log("Clearing interval with id " + int_id);
+    clearInterval(int_id);
+  }
+
+  function capture() {
+    // console.time('capture');
+    var w = video.videoWidth * scale;
+    var h = video.videoHeight * scale;
+    var qr = new QrCode();
+    qr.callback = function (result) {
+      if (/http:/.test(result)) {
+        console.log(result);
+        $("#qr-value").text(result);
+      }
+    };
+
+    try {
+      var qr_can = document.getElementById('qr-canvas').getContext('2d');
+      qr_can.drawImage(video, 0, 0, w, h);
+      var data = qr_can.getImageData(0, 0, 640, 480);
+
+      qr.decode(data);
     }
-
-    var vid_id = p_vid_id;
-    var interval = p_inter != undefined ? p_inter : 1000;
-    var scale = p_scale != undefined ? p_scale : 0.5;
-
-    var video = document.getElementById(vid_id);
-    var int_id = null;
-
-    function start() {
-        int_id = setInterval(function (video, scale) {
-            capture()
-        }, interval);
+    catch (err) {
+      $("#qr-value").text(err);
     }
+    // console.timeEnd('capture');
+  }
 
-    function stop() {
-        console.log("Clearing interval with id " + int_id);
-        clearInterval(int_id);
-    }
-
-    function capture() {
-        // console.time('capture');
-        var w = video.videoWidth * scale;
-        var h = video.videoHeight * scale;
-        var qr = new QrCode();
-        qr.callback = function (result) {
-            if (/http:/.test(result)) {
-                console.log(result)    
-            }
-        };
-
-        try {
-            var qr_can = document.getElementById('qr-canvas').getContext('2d');
-            qr_can.drawImage(video, 0, 0, w, h);
-            var data = qr_can.getImageData(0, 0, 640, 480);
-
-            qr.decode(data);
-        }
-        catch (err) {
-            $("#qr-value").text(err);
-        }
-        // console.timeEnd('capture');
-    }
-
-    return {
-        interval: interval,
-        scale: scale,
-        start: start,
-        stop: stop,
-        capture: capture
-    }
+  return {
+    interval: interval,
+    scale: scale,
+    start: start,
+    stop: stop,
+    capture: capture
+  }
 
 });
